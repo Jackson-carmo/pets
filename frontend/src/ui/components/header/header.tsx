884382209739196e785b615ee1afe2a3cec8fc3b
Jackson-carmo <jacksoncruciol157@gmail.com>
2022-06-10 18:42:37 -0300

    Instalar extensão Styled Components

---
+++ frontend/src/ui/components/header/header.tsx
@@ -0,0 +1,9 @@
+import { HeaderContainer,Logo } from "./header.style";
+
+export default function Header() {
+    return (
+        <HeaderContainer>
+            <Logo src="/img/logo.svg"/>
+        </HeaderContainer>
+    );
+}
