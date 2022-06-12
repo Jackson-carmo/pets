f6faddc8cc0db45c7cf88a22bedfe440e69aa1e2
Jackson-carmo <jacksoncruciol157@gmail.com>
2022-06-12 09:53:23 -0300

    Repository de Adoção Criado

---
+++ backend/src/main/java/com/jackson/adoteumpet/core/repositories/AdocaoRepository.java
@@ -0,0 +1,7 @@
+package com.jackson.adoteumpet.core.repositories;
+
+import com.jackson.adoteumpet.core.models.Adocao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdocaoRepository extends JpaRepository<Adocao, Long> {
+}
